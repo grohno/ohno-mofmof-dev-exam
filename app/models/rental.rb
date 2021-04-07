647288139410db1c3012e9da520e526161e70c9b
yuki ohno <jeanadler33@gmail.com>
2021-04-07 04:38:06 +0000

    物件モデルにバリデーションを設定

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,8 @@
 class Rental < ApplicationRecord
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true
+  validates :rent_property_name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :building_age, presence: true
 end
